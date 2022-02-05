c74ec9dcbd1b02830700567339a713b4587612d8
Houzhong <houzhongy@gmail.com>
2022-02-05 22:59:39 +0800

    Basic face detection AI using OpenCV

---
+++ face_detector.py
@@ -0,0 +1,25 @@
+import cv2
+
+#loading pre-trained data from opencv
+trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+#choose an image to detect face on
+img = cv2.imread("face1.jpg")
+
+#Converting the image to grayscale
+grayscaled_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#Detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+#Drawing rectangles
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+
+print(face_coordinates)
+
+cv2.imshow("Clever Programmer Face Detector", img)
+cv2.waitKey()
+
+
+print("Code Completed")
